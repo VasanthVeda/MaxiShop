dfc2f544cd852a9cf251b89f732f7e3ec3174cb2
VASANTH <VASANTH@VASANTH>
2024-04-10 15:50:29 +0530

    Add project files.

---
+++ MaxiShop.Web/Controllers/CategoryController.cs
@@ -0,0 +1,33 @@
+﻿using MaxiShop.Web.Data;
+using MaxiShop.Web.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MaxiShop.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbcontext;
+
+        public CategoryController(ApplicationDbContext dbContext)
+        {
+            _dbcontext = dbContext;
+        }
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [HttpPost]
+        public ActionResult Create([FromBody]Category category)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            _dbcontext.Category.Add(category);
+            _dbcontext.SaveChanges();
+            return Ok();
+        }
+    }
+
+}
---
+++ MaxiShop.Web/Program.cs
@@ -0,0 +1,35 @@
+using MaxiShop.Web.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+#region Database Connectivity
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+#endregion
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
